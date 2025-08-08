d96215e84c3dce00901b4f6e6461edd0d1b8e0d6
JUHO <ijuho9555@gmail.com>
2025-08-08 12:32:24 +0900

    기울어진 박스도 인식하게 코드 수정
    "어린이보호구역"이라고 적힌 글씨는 기울어져 있어서 인식이 잘 안되는 오류가 있음. 따라서 cv2.rectangle을 cv2.polylines로 바꿔 기울어진 박스도 그림. 그럼에도 인식이 잘 안돼서 threshold값을 낮춤

--- src/children_sign.py
+++ src/children_sign.py
@@ -1,28 +1,38 @@
 import easyocr
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
+# 1. OCR 리더 생성 (한글 + 영어)
 reader = easyocr.Reader(['ko', 'en'], gpu=False)
 
+# 2. 이미지 불러오기
 # result = reader.readtext('chinese_tra.jpg')
 img_path = '../img/children_sign.jpg'
 img = cv2.imread(img_path)
 
-# # 1. 이미지 불러오기
 plt.figure(figsize=(8,8))
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.show()
 
-# 2. 이미지 텍스트 인식
+# 3. 이미지 텍스트 인식
 result = reader.readtext(img_path)
 print(result)
 
-# 3. 인식된 텍스트 확인해보기
-THRESHOLD = 0.5
+# 4. 인식된 텍스트 확인해보기
+THRESHOLD = 0.01 # 신뢰도 0.5
 for bbox, text, conf in result:
     if conf >= THRESHOLD:
-        print(text)
-        cv2.rectangle(img, pt1=tuple(map(int, bbox[0])), pt2=tuple(map(int, bbox[2])), color=(0, 255, 0), thickness=2)
+        print(f'{text} (conf: {conf:.2f})')
+
+        # numpy array로 변환 (정수형 좌표)
+        pts = np.array(bbox, dtype=np.int32)
+
+        # 기울어진 박스 그리기
+        cv2.polylines(img, [pts], isClosed=True, color=(0, 255, 0), thickness=2)
+
+        # 한 번에 표시하기 위해 map과 tuple을 사용 (리스트이므로 튜플로 변환)
+        #cv2.rectangle(img, pt1=tuple(map(int, bbox[0])), pt2=tuple(map(int, bbox[2])), color=(0, 255, 0), thickness=2)
 
 plt.figure(figsize=(8,8))
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
