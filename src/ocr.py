7949ad28af67f967d79536df40ee2197d59d3bfe
JUHO <ijuho9555@gmail.com>
2025-08-08 10:40:03 +0900

    이미지 텍스트 인식해 터미널창에 출력되는지 확인

--- src/ocr.py
+++ src/ocr.py
@@ -8,6 +8,11 @@ reader = easyocr.Reader(['ch_tra', 'en'], gpu=False)
 img_path = '../img/chinese_tra.jpg'
 img = cv2.imread(img_path)
 
+# 1. 이미지 불러오기
 plt.figure(figsize=(8,8))
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.show()
+
+# 2. 이미지 텍스트 인식
+result = reader.readtext(img_path)
+print(result)
